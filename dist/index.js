e2e40ca3518719ac375abe6de05320645cf64b55
gogogo.huang <gogogohuang@gmail.com>
2017-10-10 14:57:14 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var _linebot = require('linebot');
+
+var _linebot2 = _interopRequireDefault(_linebot);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var bot = (0, _linebot2.default)({
+  channelId: "1539958657",
+  channelSecret: "e10b0956cb25191be66a7efe01131ea0",
+  channelAccessToken: "liLIIlI5QQQ3FYSbY9kOtW6sREW+cH7Rmu1mGu72Ci3Fofv9H63h1Cwzx/UiHUJ1HHhkHwZon5MNNl8e37X3oYou47I2677QWLA6VT5km3RadMa2ln59k4IuKSfrBUHIOAYV5tTceqLhHzM/MVYQbgdB04t89/1O/w1cDnyilFU="
+});
+
+bot.on('message', function (event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+var app = (0, _express2.default)();
+var linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import linebot from 'linebot';
+import express from 'express';
+
+const bot = linebot({
+  channelId: "1539958657",
+  channelSecret: "e10b0956cb25191be66a7efe01131ea0",
+  channelAccessToken: "liLIIlI5QQQ3FYSbY9kOtW6sREW+cH7Rmu1mGu72Ci3Fofv9H63h1Cwzx/UiHUJ1HHhkHwZon5MNNl8e37X3oYou47I2677QWLA6VT5km3RadMa2ln59k4IuKSfrBUHIOAYV5tTceqLhHzM/MVYQbgdB04t89/1O/w1cDnyilFU="
+});
+
+
+bot.on('message', function(event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+  });
+  
+  const app = express();
+  const linebotParser = bot.parser();
+  app.post('/', linebotParser);
+  
+  //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+  const server = app.listen(process.env.PORT || 8080, function() {
+    const port = server.address().port;
+    console.log("App now running on port", port);
+  });
