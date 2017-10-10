1aec1340bf725ae66b257c6a81e0ad6610109b21
gogogo.huang <gogogohuang@gmail.com>
2017-10-10 19:17:52 +0800

    taiwan weather

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,16 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _weatherTaiwan = require('weather-taiwan');
+
+var _weatherTaiwan2 = _interopRequireDefault(_weatherTaiwan);
+
+var _reply = require('./common/reply');
+
+var reply = _interopRequireWildcard(_reply);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var bot = (0, _linebot2.default)({
@@ -19,12 +29,23 @@ var bot = (0, _linebot2.default)({
 bot.on('message', function (event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
-    event.reply(msg).then(function (data) {
-      // success 
-      console.log(msg);
-    }).catch(function (error) {
-      // error 
-      console.log('error');
+    var replyText = void 0;
+
+    var fetcher = _weatherTaiwan2.default.fetch('CWB-27A80F1A-A586-4FDC-BE8A-641BF50848FA');
+    var parser = _weatherTaiwan2.default.parse();
+    fetcher.pipe(parser);
+
+    parser.on('data', function (data) {
+      if (data.parameters.CITY.indexOf(msg) !== 0) {
+        replyText = "現在" + msg + "溫度是" + data.elements.TEMP + " 度";
+      } else {
+        replyText = "找不到";
+      }
+
+      event.reply(replyText).then(function (data) {}).catch(function (error) {
+        // error 
+        console.log('error');
+      });
     });
   }
 });
@@ -33,7 +54,6 @@ var app = (0, _express2.default)();
 var linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
