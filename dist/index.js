5fe1428c5ae318c50b151d72b48c8c82327269e0
gogogo.huang <gogogohuang@gmail.com>
2017-10-13 00:12:02 +0800

    test code

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,10 @@ var _weatherTaiwan = require('weather-taiwan');
 
 var _weatherTaiwan2 = _interopRequireDefault(_weatherTaiwan);
 
+var _nodeFetch = require('node-fetch');
+
+var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+
 var _reply = require('./common/reply');
 
 var reply = _interopRequireWildcard(_reply);
@@ -26,26 +30,50 @@ var bot = (0, _linebot2.default)({
   channelAccessToken: "liLIIlI5QQQ3FYSbY9kOtW6sREW+cH7Rmu1mGu72Ci3Fofv9H63h1Cwzx/UiHUJ1HHhkHwZon5MNNl8e37X3oYou47I2677QWLA6VT5km3RadMa2ln59k4IuKSfrBUHIOAYV5tTceqLhHzM/MVYQbgdB04t89/1O/w1cDnyilFU="
 });
 
+var gCity = {};
+
+/**hard-code */
+var hCity = [{ id: '1', name: '台北' }, { id: '2', name: '基隆' }, { id: '3', name: '新北' }, { id: '4', name: '連江' }, { id: '5', name: '宜蘭' }, { id: '6', name: '新竹' }, { id: '8', name: '桃園' }, { id: '9', name: '苗栗' }, { id: '10', name: '台中' }, { id: '11', name: '彰化' }, { id: '12', name: '南投' }, { id: '13', name: '嘉義' }, { id: '15', name: '雲林' }, { id: '16', name: '台南' }, { id: '17', name: '高雄' }, { id: '18', name: '澎湖' }, { id: '19', name: '金門' }, { id: '20', name: '屏東' }, { id: '21', name: '台東' }, { id: '22', name: '花蓮' }];
+
+/**end */
+function getAllCity() {
+  (0, _nodeFetch2.default)('https://works.ioa.tw/weather/api/all.json', {
+    method: 'GET',
+    mode: 'cors'
+  }).then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  }).then(function (data) {
+    Object.assign(gCity, data);
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
 bot.on('message', function (event) {
   if (event.message.type = 'text') {
-    var msg = event.message.text;
-
-    var fetcher = _weatherTaiwan2.default.fetch('CWB-27A80F1A-A586-4FDC-BE8A-641BF50848FA');
-    var parser = _weatherTaiwan2.default.parse();
-
-    fetcher.pipe(parser);
-
-    parser.on('data', function (data) {
-      //console.log(data);
-      // console.log(data.locationName.search(msg));   
-      if (encodeURIComponent(data.locationName).search(encodeURIComponent(msg)) === 0) {
-        var replyText = "現在" + msg + "溫度是" + data.elements.TEMP + " 度";
-        // console.log(replyText);
-        event.reply(replyText).then(function (data) {}).catch(function (error) {
-          // error 
-          console.log('error');
-        });
+    var city = hCity.find(function (x) {
+      if (x.name === event.message.text) {
+        return true;
       }
+    });
+    console.log(city);
+    (0, _nodeFetch2.default)('https://works.ioa.tw/weather/api/weathers/' + city.id + '.json', {
+      method: 'GET',
+      mode: 'cors'
+    }).then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    }).then(function (data) {
+      console.log(data);
+      var replyText = '\u73FE\u5728\u5929\u6C23' + data.desc + '氣溫' + data.temperature + '度';
+      event.reply(replyText).then(function (data) {}).catch(function (error) {
+        console.log('error');
+      });
+    }).catch(function (err) {
+      console.log(err);
     });
   }
 });
@@ -55,6 +83,7 @@ var linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function () {
+  getAllCity();
   var port = server.address().port;
   console.log("App now running on port", port);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import linebot from 'linebot';
 import express from 'express';
 import weatherTW from 'weather-taiwan';
+import fetch from 'node-fetch';
 
 import * as reply from './common/reply';
 
@@ -10,28 +11,81 @@ const bot = linebot({
   channelAccessToken: "liLIIlI5QQQ3FYSbY9kOtW6sREW+cH7Rmu1mGu72Ci3Fofv9H63h1Cwzx/UiHUJ1HHhkHwZon5MNNl8e37X3oYou47I2677QWLA6VT5km3RadMa2ln59k4IuKSfrBUHIOAYV5tTceqLhHzM/MVYQbgdB04t89/1O/w1cDnyilFU="
 });
 
+let gCity = {};
+
+/**hard-code */
+const hCity = [
+  { id: '1', name: '台北' },
+  { id: '2', name: '基隆' },
+  { id: '3', name: '新北' },
+  { id: '4', name: '連江' },
+  { id: '5', name: '宜蘭' },
+  { id: '6', name: '新竹' },
+  { id: '8', name: '桃園' },
+  { id: '9', name: '苗栗' },
+  { id: '10', name: '台中' },
+  { id: '11', name: '彰化' },
+  { id: '12', name: '南投' },
+  { id: '13', name: '嘉義' },
+  { id: '15', name: '雲林' },
+  { id: '16', name: '台南' },
+  { id: '17', name: '高雄' },
+  { id: '18', name: '澎湖' },
+  { id: '19', name: '金門' },
+  { id: '20', name: '屏東' },
+  { id: '21', name: '台東' },
+  { id: '22', name: '花蓮' },
+];
+
+/**end */
+function getAllCity() {
+  fetch(
+    `https://works.ioa.tw/weather/api/all.json`, {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      Object.assign(gCity, data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
 bot.on('message', function (event) {
   if (event.message.type = 'text') {
-    const msg = event.message.text;
-
-    const fetcher = weatherTW.fetch('CWB-27A80F1A-A586-4FDC-BE8A-641BF50848FA');
-    const parser = weatherTW.parse();
-    
-    fetcher.pipe(parser);
-
-    parser.on('data', function (data) {
-      //console.log(data);
-      // console.log(data.locationName.search(msg));   
-      if (encodeURIComponent(data.locationName).search(encodeURIComponent(msg)) === 0) {
-        const replyText = "現在" + msg + "溫度是" + data.elements.TEMP + " 度";
-        // console.log(replyText);
-        event.reply(replyText).then(function (data) {
-        }).catch(function (error) {
-          // error 
-          console.log('error');
-        });
+    const city = hCity.find((x)=>{
+      if(x.name === event.message.text){
+        return true;
       }
     });
+    console.log(city);
+    fetch(
+      `https://works.ioa.tw/weather/api/weathers/${city.id}.json`, {
+        method: 'GET',
+        mode: 'cors',
+      })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        const replyText = `現在天氣` + data.desc + '氣溫' + data.temperature + '度';
+        event.reply(replyText).then(function (data) {
+        }).catch(function (error) {
+          console.log('error');
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 });
 
@@ -40,6 +94,7 @@ const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 const server = app.listen(process.env.PORT || 8080, function () {
+  getAllCity();
   const port = server.address().port;
   console.log("App now running on port", port);
 });
