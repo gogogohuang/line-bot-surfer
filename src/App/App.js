b0911cb245e6a733b46f2a817dae1c8574d1bdcc
gogogohuang <gogogohuang@gmail.com>
2020-01-07 22:52:56 +0800

    tmp

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
+
+class App extends Component {
+  render() {
+    return <div>今天就開始學React!</div>;
+  }
+}
+
+export default App;
